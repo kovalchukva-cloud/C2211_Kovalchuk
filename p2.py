abcc02eec3ac0b153ccb760032d068685bb4278a
Your Name <your_email@whatever.com>
2025-11-29 11:51:34 +0200

    para2 Student Sim

--- p2.py
+++ p2.py
@@ -1,12 +1,13 @@
-    import random
+import random
 
-    class Student:
-        def _init_(selfself, name):
+class Student:
+        def __init__(self, name):
             self.name = name
+            self.gladness = 50
             self.progress = 0
             self.alive = True
 
-        def to study(self):
+        def to_study(self):
             print("Time to study")
             self.progress += 0,12
             self.gladness -= 5
@@ -24,34 +25,34 @@
             if self.progress < -0.5:
                 print("Cst out...")
                 self.alive = False
-            elif self.gladnesss < 0:
+            elif self.gladness < 0:
                 print("Depression")
                 self.alive = False
             elif self.progress > 5:
                 print("Passed externally")
-                self,.alive = False
+                self.alive = False
 
-            def end_of_day(self):
-                print(f"Gladness = {self.gladness}")
-                print(f"Progress = {round(self.progress, 2)}")
+        def end_of_day(self):
+            print(f"Gladness = {self.gladness}")
+            print(f"Progress = {round(self.progress, 2)}")
 
-            def live(self, day):
-                day = f"Day {day} of {self.name} life"
-                print(f"{day:=^50}")
-                live_cube = random.randint (a:1, b:3)
-                if live_cube = 1:
-                    self.to_study()
-                elif live_cube 2:
-                    self.to_sleep():
-                elif live_cube = 3:
-                    self.to_chill()
-                self.end_of_day()
-                self.is_alive()
+        def live(self, day):
+            day = f"Day {day} of {self.name} life"
+            print(f"{day:=^50}")
+            live_cube = random.randint (1, 3)
+            if live_cube == 1:
+                self.to_study()
+            elif live_cube == 2:
+                self.to_sleep()
+            elif live_cube == 3:
+                self.to_chill()
+            self.end_of_day()
+            self.is_alive()
 
 
-    student1 = Student(name="Vasya")
+student1 = Student(name="Vasya")
 
-    for day in range(365):
-        if student.alive = False:
-            break
-        student1.live(day)
+for day in range(365):
+    if student1.alive == False:
+        break
+    student1.live(day)
